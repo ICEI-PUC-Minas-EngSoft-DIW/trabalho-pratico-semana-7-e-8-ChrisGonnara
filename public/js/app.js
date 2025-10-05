6311ed2af7e8690d35539e9c5804201eecff744c
Christiano Gonçalves <chrispeixe00@gmail.com>
2025-10-05 12:20:11 -0300

    parte 2

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+const cards = [
+    { 
+        id: 1,
+        titulo: "Resenhas Detalhadas",
+        descricao: "Análises completas dos livros",
+        icone: "fa-book-open",
+        link: "#",
+    },
+    { 
+        id: 2,
+        titulo: "Biografia de autores",
+        descricao: "Conheça a história dos autores",
+        icone: "fa-feather-pointed",
+        link: "#",
+    },
+    { 
+        id: 3,
+        titulo: "Guias de leitura",
+        descricao: "Dicas para aproveitar melhor suas leituras",
+        icone: "fa-compass",
+        link: "#",
+    },
+];
+
+const container = document.getElementById('cards-container');
+
+cards.forEach(card => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'col-12 col-md-4 mb-4'; // mantém layout responsivo e coluna igual Bootstrap
+    cardDiv.innerHTML = `
+        <div class="card h-100 text-center shadow-sm card-hover">
+        <div class="card-icon my-3">
+        <i class="fa-solid ${card.icone}"></i>
+    </div>
+    <div class="card-body">
+        <h3 class="card-title">${card.titulo}</h3>
+        <p class="card-text">${card.descricao}</p>
+        <a href="${card.link}" class="btn btn-primary">Saiba mais</a>
+        </div>
+    </div>
+    `;
+container.appendChild(cardDiv);
+});
